d01144a0c2692538bab80d12635fdb507eec1a17
Oluwadunsin david <oluwadunsindavid21@gmail.com>
2025-11-21 09:27:03 +0100

    `Updated FeaturedInterview component and added title and content sections comment`

--- src/components/HomeComp/FeaturedInterview.jsx
+++ src/components/HomeComp/FeaturedInterview.jsx
@@ -3,13 +3,15 @@ import React from "react";
 const FeaturedInterview = () => {
   return (
     <div className="mt-20">
+      {/* title */}
       <div className="mb-10">
         <h1 className="text-4xl font-bold text-center">
           <span className="text-black">Featured Interview</span>
         </h1>
         <div className="w-28 h-1 bg-orange-500 mx-auto mt-4"></div>
       </div>
-      <div className="px-24 w-full flex items-center justify-center py-20 md:py-0">
+      {/* content */}
+      <div className="px-4 sm:px-12 md:px-24 w-full flex items-center justify-center py-20 md:py-0">
         <div className="w-full max-w-4xl h-[350px] md:h-[450px] lg:h-[600px] rounded-xl overflow-hidden shadow-lg relative">
           <iframe
             className="absolute top-0 left-0 w-full h-full"
